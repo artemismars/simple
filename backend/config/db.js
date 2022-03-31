763ec6fb277691ab6b1046e5c470ba8a3cde5fda
artemismars <artemismars@outlook.com>
2022-04-01 00:24:33 +0900

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const URI = process.env.DATABASE_URI || "mongodb://localhost:27017/simple_dev";
 
-mongoose.connect(URI).catch((reason) => {
-  console.log(reason);
-});
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("connection is successful");
+  })
+  .catch((reason) => {
+    console.log(reason);
+  });
